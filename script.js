8144873b76fc96f76871ed47fda29b1d2bfc0087
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2024-10-01 18:48:13 -0300

    inicio logica bonus

--- script.js
+++ script.js
@@ -5,21 +5,29 @@ let inputPlanet = document.getElementById('inputPlanet');
 let planetsSearch = document.getElementById('planetsSearch');
 
 async function listPlanets() {
-  let result = await fetch('https://swapi.dev/api/planets/')
-  let {results} = await result.json();
-  console.log(results)
+  let allPlanets = [];
+  let url = 'https://swapi.dev/api/planets/';
+
+  while (url) {
+    let result = await fetch(url);
+    let {results, next} = await result.json();
+    
+    allPlanets = allPlanets.concat(results); 
+    url = next; 
+  }
   
-  results.forEach(planet => {
+  allPlanets.forEach(planet => {
 
     let li = document.createElement('li');
 
-    li.innerHTML = `<button onclick="showData('${planet.name}','${planet.climate}','${planet.population}','${planet.terrain}')">${planet.name}</button>`
+    li.innerHTML = `<button onclick="showData('${planet.name}','${planet.climate}','${planet.population}',
+    '${planet.terrain}','${planet.residents}')">${planet.name}</button>`
                          
     buttonlist.appendChild(li)
   });
 }
 
-function showData(planetName, planetClimate, planetPopulation, planetTerrain){
+function showData(planetName, planetClimate, planetPopulation, planetTerrain,planetResidents){
   let li = document.createElement('li');
 
     li.innerHTML = `<div>
@@ -27,6 +35,7 @@ function showData(planetName, planetClimate, planetPopulation, planetTerrain){
                       <p>Clima: ${planetClimate}</p>
                       <p>População: ${planetPopulation} habitantes</p>
                       <p>Tipo de terreno: ${planetTerrain}</p>
+                      <p>Habitantes mais famosos: ${planetResidents}</p>
                      </div>`
                          
     planetsData.appendChild(li)
@@ -43,13 +52,8 @@ async function searchPlanets() {
     if (planet.name.toLowerCase() === inputPlanet.value.toLowerCase()) {
     
 
-    li.innerHTML = `<div>
-                    <h2>Nome: ${planet.name}</h2>
-                    <p>Clima: ${planet.climate}</p>
-                    <p>População: ${planet.population} habitantes</p>
-                    <p>Tipo de terreno: ${planet.terrain}</p>
-                   </div>`
-    planetsSearch.appendChild(li)
+    showData(planet.name, planet.climate, planet.population, planet.terrain, planet.residents);
+    
     }
     
 
