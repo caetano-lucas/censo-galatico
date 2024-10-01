d54b54f5647a9443c3c6165eef371e9a55e7c225
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2024-10-01 17:01:29 -0300

    tarefa 3, ao clicar no botao exibe dados do planeta

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 let buttonlist = document.getElementById('buttonlist');
+let planetsData = document.getElementById('planetsData');
 
 async function listPlanets() {
   let result = await fetch('https://swapi.dev/api/planets/')
@@ -10,9 +11,22 @@ async function listPlanets() {
 
     let li = document.createElement('li');
 
-    li.innerHTML = `<button>${planet.name}</button>`
+    li.innerHTML = `<button onclick="showData('${planet.name}','${planet.climate}','${planet.population}','${planet.terrain}')">${planet.name}</button>`
                          
     buttonlist.appendChild(li)
   });
 }
 
+function showData(planetName, planetClimate, planetPopulation, planetTerrain){
+  let li = document.createElement('li');
+
+    li.innerHTML = `<div>
+                      <h2>Nome: ${planetName}</h2>
+                      <p>Clima: ${planetClimate}</p>
+                      <p>População: ${planetPopulation} habitantes</p>
+                      <p>Tipo de terreno: ${planetTerrain}</p>
+                     </div>`
+                         
+    planetsData.appendChild(li)
+
+}
