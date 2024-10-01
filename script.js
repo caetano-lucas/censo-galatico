29df9c32144c4e15e7b373199bef7f1719428dfe
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2024-10-01 16:27:54 -0300

    tarefa 2, para cada planeta criar um botao na tela

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 
+let buttonlist = document.getElementById('buttonlist');
+
 async function listPlanets() {
   let result = await fetch('https://swapi.dev/api/planets/')
   let {results} = await result.json();
   console.log(results)
   
+  results.forEach(planet => {
+
+    let li = document.createElement('li');
+
+    li.innerHTML = `<button>${planet.name}</button>`
+                         
+    buttonlist.appendChild(li)
+  });
 }
+
